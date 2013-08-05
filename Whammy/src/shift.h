067b07a33aea2c334054fcda980634c27f64b824
Andre <andre@andrebian.agr>
2013-08-05 16:45:44 -0300

    new Whammy

---
+++ Whammy/src/shift.h
@@ -0,0 +1,5 @@
+#include <complex>
+#include <cmath>
+#include <fftw3.h>
+using namespace std;
+void shift(int N, int hopa, int hops, double *frames, double *w, complex<double> *XaPrevious, double *PhiPrevious, double **Q, double *yshift, complex<double> *Xa, complex<double> *Xs, double *q, complex<double> *qaux, complex<double> *framesaux, double *Phi, double *ysaida, double *ysaida2, int Qcolumn);
