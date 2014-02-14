4a2b25ef8aa56132549912992aec282ad370463c
André <mrcouts@gmail.com>
2014-02-14 18:33:10 -0200

    OPTIMIZATION!

--- Shared_files/shift.cpp
+++ Shared_files/shift.cpp
@@ -13,6 +13,8 @@ using namespace arma;
 
 void shift(int N, int hopa, int *hops, double *frames, cx_vec *frames2, vec *w, cx_vec *XaPrevious, vec *Xa_arg, vec *Xa_abs, vec *XaPrevious_arg, vec *PhiPrevious, mat *Q, double *yshift, cx_vec *Xa, cx_vec *Xs, vec *q, cx_vec *qaux, vec *Phi, double *ysaida, double *ysaida2, int Qcolumn, vec *d_phi, vec *d_phi_prime, vec *d_phi_wrapped, vec *omega_true_sobre_fs, vec *I, vec *AUX, fftw_plan p, fftw_plan p2)
 {
+	static bool first = true;
+	
 	//Some declaration
 	int L;
 	L = N;
@@ -28,11 +30,6 @@ void shift(int N, int hopa, int *hops, double *frames, cx_vec *frames2, vec *w, 
 	double n3;
 	
 	//Some inicialization
-	
-	for (int i=1; i<=L; i++)
-	{
-		ysaida[i-1] = 0;
-	}
 	
 	ysaida2 = &ysaida[L-N];
 	
@@ -75,22 +72,26 @@ void shift(int N, int hopa, int *hops, double *frames, cx_vec *frames2, vec *w, 
 	fftw_execute(p2);
 	q[0] = real(qaux[0])/N;
 	q[0] = q[0] % w[0]/(sqrt((N/(2*hops[Qcolumn-1]))));
-	for (int j=1; j<=(Qcolumn-1); j++)
-	{
-		Q[0].col(j-1) = Q[0].col(j);
-	}
 	
-	Q[0].col(Qcolumn-1) = q[0];
-	
-	for (int k=1; k <= Qcolumn; k++)
+	if ( first == true)
 	{
-		for (int i=1; i<=N; i++)
+		first = false;
+		for (int i=1; i<=L-N; i++)
 		{
-			ysaida[i+Hops-1] = ysaida[i+Hops-1] + Q[0](i-1,k-1);
+			ysaida[i-1] = 0;
 		}
-		Hops = Hops + hops[k-1];
+		for (int i=L-N+1; i<=L; i++)
+		{
+			ysaida[i-1] = q[0](i-(L-N+1));
+		}
 	}
-	
+	else
+	{
+		for (int i=L-N+1; i<=L; i++)
+		{
+			ysaida[i-1] = ysaida[i-1] + q[0](i-(L-N+1));
+		}
+	}
 	//Linear interpolation
 	r = ((double)hops[Qcolumn-1])/((double)hopa);
 
@@ -101,4 +102,15 @@ void shift(int N, int hopa, int *hops, double *frames, cx_vec *frames2, vec *w, 
             n2 = ceil(n3);
             yshift[n-1] = ysaida2[n1-1] + ((ysaida2[n2-1]-ysaida2[n1-1]))*(n3 - (double)n1);
 		}
+		
+	//Shift ysaida hops[0] left
+	for (int i=1; i<=L-hops[0]; i++)
+	{
+		ysaida[i-1] = ysaida[i-1+hops[0]];
+	}
+	for (int i=L-hops[0]+1; i<=L; i++)
+	{
+		ysaida[i-1] = 0;
+	}
+	
 }
