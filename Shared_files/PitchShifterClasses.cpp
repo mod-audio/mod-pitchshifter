415db4f9e73000904f751057960c05cef62becc5
André <mrcouts@gmail.com>
2014-10-27 21:18:14 -0200

    Drop: Gambs ok

--- Shared_files/PitchShifterClasses.cpp
+++ Shared_files/PitchShifterClasses.cpp
@@ -2,54 +2,63 @@
 
 PSAnalysis::PSAnalysis(uint32_t n_samples, int nBuffers) //Construtor
 {
+	cout << "Entrei no contrutor do PSAnalysis" << endl;
 	Qcolumn = nBuffers;
 	hopa = n_samples;
 	N = nBuffers*n_samples;
 
-	frames = new double[N]; for (int i = 0; i < N; i++) frames[i] = 0;
-	b = new double*[hopa];
+	frames = (double*)calloc(N,sizeof(double));
+	b = (double**)calloc(hopa,sizeof(double*));
+
 
 	for (int i=0 ; i< nBuffers; i++)
     {
 		b[i] = &frames[i*hopa];
 	}
 
+
 	frames2 = fftwf_alloc_real(N); for (int i = 0; i < N; i++) frames2[i] = 0;
 	fXa = fftwf_alloc_complex(N/2 + 1);
-	Xa->zeros(N/2 + 1);
-	XaPrevious->zeros(N/2 + 1);
-	Xa_arg->zeros(N/2 + 1);
-	XaPrevious_arg->zeros(N/2 + 1);
-	d_phi->zeros(N/2 + 1);
-	d_phi_prime->zeros(N/2 + 1);
-	d_phi_wrapped->zeros(N/2 + 1);
-	omega_true_sobre_fs->zeros(N/2 + 1);
-	AUX->zeros(N/2 + 1);
-	Xa_abs->zeros(N/2 + 1);
-	w->zeros(N); hann(N,w);
-	I->zeros(N/2 + 1); *I = linspace(0, N/2, N/2 + 1);
+	Xa.zeros(N/2 + 1);
+	XaPrevious.zeros(N/2 + 1);
+	Xa_arg.zeros(N/2 + 1);
+	XaPrevious_arg.zeros(N/2 + 1);
+	d_phi.zeros(N/2 + 1); 
+	d_phi_prime.zeros(N/2 + 1); 
+	d_phi_wrapped.zeros(N/2 + 1); 
+	omega_true_sobre_fs.zeros(N/2 + 1); 
+	AUX.zeros(N/2 + 1); 
+	Xa_abs.zeros(N/2 + 1); 
+	w.zeros(N); hann(N,&w); 
+	I.zeros(N/2 + 1); I = linspace(0, N/2, N/2 + 1); 
 	p = fftwf_plan_dft_r2c_1d(N, frames2, fXa, FFTW_ESTIMATE);
+
+	cout << "Sai do contrutor do PSAnalysis" << endl;
+
 }
 
 PSAnalysis::~PSAnalysis() //Destrutor
 {
-	delete[] b;
-	delete[] frames;
-	delete[] frames2;
-	fftwf_free(fXa);
-	Xa->clear();
-	XaPrevious->clear();
-	Xa_arg->clear();
-	XaPrevious_arg->clear();
-    d_phi->clear();
-	d_phi_prime->clear();
-	d_phi_wrapped->clear();
-	omega_true_sobre_fs->clear();
-	AUX->clear();
-	Xa_abs->clear();
-	w->clear();
-	I->clear();
-	fftwf_destroy_plan(p);
+	cout << "Entrei no destrutor do PSAnalysis" << endl;
+	fftwf_destroy_plan(p); cout << "1" << endl;
+	free(b); cout << "2" << endl;
+	free(frames); cout << "3" << endl;
+	fftwf_free(frames2); cout << "4" << endl;
+	fftwf_free(fXa); cout << "5" << endl;
+	Xa.clear(); cout << "6" << endl;
+	XaPrevious.clear(); cout << "7" << endl;
+	Xa_arg.clear(); cout << "8" << endl;
+	XaPrevious_arg.clear(); cout << "9" << endl;
+    d_phi.clear(); cout << "10" << endl;
+	d_phi_prime.clear(); cout << "11" << endl;
+	d_phi_wrapped.clear(); cout << "12" << endl;
+	omega_true_sobre_fs.clear(); cout << "13" << endl;
+	AUX.clear(); cout << "14" << endl;
+	Xa_abs.clear(); cout << "15" << endl;
+	w.clear(); cout << "16" << endl;
+	I.clear();
+
+	cout << "Sai do destrutor do PSAnalysis" << endl;
 }
 
 void PSAnalysis::PreAnalysis(int nBuffers, float *in)
@@ -72,7 +81,7 @@ void PSAnalysis::Analysis()
 	
 	for (int i=1; i<=N; i++)
 	{
-		frames2[i-1] = frames[i-1]*w[0](i-1)/(sqrt(((double)N/(2*(double)hopa))));
+		frames2[i-1] = frames[i-1]*w(i-1)/(sqrt(((double)N/(2*(double)hopa))));
 	}
 	
 	/*Analysis*/
@@ -82,38 +91,43 @@ void PSAnalysis::Analysis()
 	
 	for (int i=1; i<=(N/2 + 1); i++)
 	{
-		Xa[0](i-1) = cx_float(fXa[i-1][0], fXa[i-1][1]);
-		Xa_arg[0](i-1) = angle(Xa[0](i-1));
+		Xa(i-1) = cx_float(fXa[i-1][0], fXa[i-1][1]);
+		Xa_arg(i-1) = angle(Xa(i-1));
 	}
-	d_phi[0] = Xa_arg[0] - XaPrevious_arg[0];
-	d_phi_prime[0] = d_phi[0] - ((2*M_PI * hopa) / N) * I[0];
-	AUX[0] = floor((d_phi_prime[0] + M_PI) / (2*M_PI));
-	d_phi_wrapped[0] = d_phi_prime[0] - AUX[0] * (2*M_PI);
-	omega_true_sobre_fs[0] = (2*M_PI/N) * I[0] + d_phi_wrapped[0] / hopa;
+	d_phi = Xa_arg - XaPrevious_arg;
+	d_phi_prime = d_phi - ((2*M_PI * hopa) / N) * I;
+	AUX = floor((d_phi_prime + M_PI) / (2*M_PI));
+	d_phi_wrapped = d_phi_prime - AUX * (2*M_PI);
+	omega_true_sobre_fs = (2*M_PI/N) * I + d_phi_wrapped / hopa;
 
-	Xa_abs[0] = abs(Xa[0]);
-	XaPrevious[0] = Xa[0];
-	XaPrevious_arg[0] = Xa_arg[0];
+	Xa_abs = abs(Xa);
+	XaPrevious = Xa;
+	XaPrevious_arg = Xa_arg;
 }
 
 PSSinthesis::PSSinthesis(PSAnalysis *obj) //Construtor
 {
+	cout << "Entrei no construtor do PSSinthesis" << endl;
 	Qcolumn = obj->Qcolumn;
 	hopa = obj->hopa;
 	N = obj->N;
-	omega_true_sobre_fs = obj->omega_true_sobre_fs;
-	Xa_abs = obj->Xa_abs;
-	w = obj->w;
+	omega_true_sobre_fs = &obj->omega_true_sobre_fs;
+	Xa_abs = &obj->Xa_abs;
+	w = &obj->w;
 
-	hops = new int[Qcolumn]; for (int i = 0; i < Qcolumn;i++) hops[i] = hopa;
-	ysaida = new double[2*N + 4*(Qcolumn-1)*hopa]; for (int i = 0; i < 2*N + 4*(Qcolumn-1)*hopa ;i++) ysaida[i] = 0;
-	yshift = new double[hopa]; for (int i = 0; i < hopa ;i++) yshift[i] = 0;
-	q = fftwf_alloc_real(N);
-	fXs = fftwf_alloc_complex(N/2 + 1);
-	Xs->zeros(N/2 + 1);
-	Phi->zeros(N/2 + 1);
-	PhiPrevious->zeros(N/2 + 1);
-	p2 = fftwf_plan_dft_c2r_1d(N, fXs, q, FFTW_ESTIMATE);
+	cout << "?" << endl;
+
+	hops = new int[Qcolumn]; for (int i = 0; i < Qcolumn;i++) hops[i] = hopa; cout << "1" << endl;
+	ysaida = new double[2*N + 4*(Qcolumn-1)*hopa]; for (int i = 0; i < 2*N + 4*(Qcolumn-1)*hopa ;i++) ysaida[i] = 0; cout << "2" << endl;
+	yshift = new double[hopa]; for (int i = 0; i < hopa ;i++) yshift[i] = 0; cout << "3" << endl;
+	q = fftwf_alloc_real(N); cout << "4" << endl;
+	fXs = fftwf_alloc_complex(N/2 + 1); cout << "5" << endl;
+	Xs.zeros(N/2 + 1); cout << "6" << endl;
+	Phi.zeros(N/2 + 1); cout << "7" << endl;
+	PhiPrevious.zeros(N/2 + 1); cout << "8" << endl;
+	p2 = fftwf_plan_dft_c2r_1d(N, fXs, q, FFTW_ESTIMATE); cout << "9" << endl;
+
+	cout << "Sai do construtor do PSSinthesis" << endl;
 }
 
 PSSinthesis::~PSSinthesis() //Destrutor
@@ -123,9 +137,9 @@ PSSinthesis::~PSSinthesis() //Destrutor
 	delete[] yshift;
 	fftwf_free(q);
 	fftwf_free(fXs);
-	Xs->clear();
-	Phi->clear();
-	PhiPrevious->clear();
+	Xs.clear();
+	Phi.clear();
+	PhiPrevious.clear();
 	fftwf_destroy_plan(p2);
 }
 
@@ -158,19 +172,19 @@ void PSSinthesis::Sinthesis(double s)
 
 	// We can divide the code in two here
 
-	Phi[0] = PhiPrevious[0] + (hops[Qcolumn-1])*omega_true_sobre_fs[0] ;
+	Phi = PhiPrevious + (hops[Qcolumn-1])*omega_true_sobre_fs[0] ;
 	for (int i=1; i<=(N/2 + 1); i++)
 	{
-        Xs[0](i-1) = ExponencialComplexa(Phi[0](i-1));
+        Xs(i-1) = ExponencialComplexa(Phi(i-1));
 	}
-	Xs[0] = Xa_abs[0] % Xs[0];
-	PhiPrevious[0] = Phi[0];
+	Xs = Xa_abs[0] % Xs;
+	PhiPrevious = Phi;
 	
 	
 	for (int i=1; i<=(N/2 + 1); i++)
 	{
-        fXs[i-1][0] = real(Xs[0](i-1));
-        fXs[i-1][1] = imag(Xs[0](i-1));
+        fXs[i-1][0] = real(Xs(i-1));
+        fXs[i-1][1] = imag(Xs(i-1));
 	}
 	
 	/*Synthesis*/
--- Shared_files/PitchShifterClasses.h
+++ Shared_files/PitchShifterClasses.h
@@ -26,21 +26,21 @@ public:
     double **b;
     
     double *frames; //A frame of last N samples
-    vec *w; //A hanning window vector
+    vec w; //A hanning window vector
     float *frames2; //It's the frames vector windowed
     fftwf_plan p; //FFTW plan for the FFT of frames2
     fftwf_complex *fXa; // FFT of frames2
-    cx_vec *Xa; // Armadillo vector of fXa
-    vec *Xa_arg; //Phase of Xa
-    vec *Xa_abs; //Modulus of Xa
-    cx_vec *XaPrevious;
-    vec *XaPrevious_arg;
-    vec *d_phi; //Xa_arg - XaPrevious
-	vec *d_phi_prime; //?
-	vec *I; // A vector of the N/2 integer numbers, starting from 0.
-	vec *AUX; //?
-	vec *d_phi_wrapped; //?
-	vec *omega_true_sobre_fs; //?
+    cx_vec Xa; // Armadillo vector of fXa
+    vec Xa_arg; //Phase of Xa
+    vec Xa_abs; //Modulus of Xa
+    cx_vec XaPrevious;
+    vec XaPrevious_arg;
+    vec d_phi; //Xa_arg - XaPrevious
+	vec d_phi_prime; //?
+	vec I; // A vector of the N/2 integer numbers, starting from 0.
+	vec AUX; //?
+	vec d_phi_wrapped; //?
+	vec omega_true_sobre_fs; //?
 };
 
 class PSSinthesis
@@ -60,9 +60,9 @@ public:
 
 
     int *hops; //The last Qcolumn's hop's used in the overlap-add
-    vec *Phi; //The synthesized phase
-	vec *PhiPrevious;
-	cx_vec *Xs; //The synthesized spectrum, with modulus Xa_abs and phase Phi
+    vec Phi; //The synthesized phase
+	vec PhiPrevious;
+	cx_vec Xs; //The synthesized spectrum, with modulus Xa_abs and phase Phi
 	fftwf_complex *fXs; //fftw version of Xs
 	fftwf_plan p2; //FFTW plan for the IFFT of fXs
 	float *q; //windowed IFFT of fXs
