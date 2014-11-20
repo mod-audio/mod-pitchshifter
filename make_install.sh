40883b2d8fdcc48cdcfec7c392ebbbdf665d71dc
André <mrcouts@gmail.com>
2014-11-20 20:04:32 -0200

    Some uncommented a impotante part!

--- make_install.sh
+++ make_install.sh
@@ -23,6 +23,6 @@ cd SuperWhammy
 make install
 cd ..
 mkdir -p /etc/fftw/wisdom
-#echo "Creating fftw wisdom file"
-#cd /etc/fftw/wisdom
-#fftwf-wisdom -n -x -o my.wisdom rof1024 rob1024 rof1536 rob1536 rof2048 rob2048 rof2176 rob2176 rof2304 rob2304 rof2432 rob2432 rof2560 rob2560 rof3072 rob3072 rof4096 rob4096
+echo "Creating fftw wisdom file"
+cd /etc/fftw/wisdom
+fftwf-wisdom -n -x -o my.wisdom rof1024 rob1024 rof1536 rob1536 rof2048 rob2048 rof2176 rob2176 rof2304 rob2304 rof2432 rob2432 rof2560 rob2560 rof3072 rob3072 rof4096 rob4096
